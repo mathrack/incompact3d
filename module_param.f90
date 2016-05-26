a79635c5db2a746e17bd7cbf5ec5c7614529caee
cedricfla <cedric.flageul@protonmail.com>
2016-05-26 14:57:20 +0200

    Save attribute for all variables in the module param.

--- module_param.f90
+++ module_param.f90
@@ -134,19 +134,19 @@ module param
 
 use decomp_2d, only : mytype
 
-  integer :: nclx,ncly,nclz
-  integer :: ifft, ivirt,istret,iforc_entree,iturb
-  integer :: itype, iskew, iin, nscheme, ifirst, ilast, iles
-  integer :: isave,ilit,idebmod, imodulo, idemarre, icommence, irecord
-  integer :: iscalar
-  integer :: nxboite, istat,iread,iadvance_time 
-  real(mytype) :: xlx,yly,zlz,dx,dy,dz,dx2,dy2,dz2
-  real(mytype) :: dt,xnu,noise,noise1,pi,twopi,u1,u2,sc
-  real(mytype) :: t,xxk1,xxk2
-  integer :: itr,itime
-  character :: filesauve*80, filenoise*80, &
+  integer, save :: nclx,ncly,nclz
+  integer, save :: ifft, ivirt,istret,iforc_entree,iturb
+  integer, save :: itype, iskew, iin, nscheme, ifirst, ilast, iles
+  integer, save :: isave,ilit,idebmod, imodulo, idemarre, icommence, irecord
+  integer, save :: iscalar
+  integer, save :: nxboite, istat,iread,iadvance_time 
+  real(mytype), save :: xlx,yly,zlz,dx,dy,dz,dx2,dy2,dz2
+  real(mytype), save :: dt,xnu,noise,noise1,pi,twopi,u1,u2,sc
+  real(mytype), save :: t,xxk1,xxk2
+  integer, save :: itr,itime
+  character, save :: filesauve*80, filenoise*80, &
        nchamp*80,filepath*80, fileturb*80, filevisu*80 
-  real(mytype), dimension(5) :: adt,bdt,cdt,gdt
+  real(mytype), dimension(5), save :: adt,bdt,cdt,gdt
 end module param
 
 module IBM
