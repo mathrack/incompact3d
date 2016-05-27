138e254fdf36489b47aefcd314e624c04b251b5b
cedricfla <cedric.flageul@protonmail.com>
2016-05-27 09:26:46 +0200

    Imposed flow rate set to unity in the subroutine channel.

--- tools.f90
+++ tools.f90
@@ -892,27 +892,27 @@ ut3=0.
 do k=1,ysize(3)
 do i=1,ysize(1)
    ut=0.
-   do j=1,ny-1
+   do j=1,ysize(2)-1
       if (istret.ne.0) ut=ut+(yp(j+1)-yp(j))*(ux(i,j+1,k)-0.5*(ux(i,j+1,k)-ux(i,j,k)))
       if (istret.eq.0) ut=ut+(yly/(ny-1))*(ux(i,j+1,k)-0.5*(ux(i,j+1,k)-ux(i,j,k)))
    enddo
-   ut=ut/yly
+   ut=ut/(yp(ysize(2))-yp(1))
    ut3=ut3+ut
 enddo
 enddo
-ut3=ut3/ysize(1)/ysize(3)
 
 call MPI_ALLREDUCE(ut3,ut4,1,real_type,MPI_SUM,MPI_COMM_WORLD,code)
-ut4=ut4/nproc
+ut4=ut4/dble(xsize(1)*zsize(3))
 
 !can=-2.*xnu*gdt(itr) ! Poisseuille    
-can=-(2./3.-ut4) ! constant flow rate
+can=-(1.-ut4) ! constant flow rate
+can=can*yly*2./(yp(ny)+yp(ny-1)-yp(2)-yp(1)) ! 
 
 if (nrank==0) print *,nrank,'UT',ut4,can
 
 do k=1,ysize(3)
+do j=2,ysize(2)-1
 do i=1,ysize(1)
-do j=2,ny-1
    ux(i,j,k)=-can+ux(i,j,k)
 enddo
 enddo
